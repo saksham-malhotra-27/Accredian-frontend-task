83a83ada279e9c6ff6d345f2f3e7e1808b2e9092
Saksham <saksh.m.malhotra@gmail.com>
2024-07-20 14:45:32 +0530

    set Loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -8,8 +8,9 @@ const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ loading }) => {
   if (!loading) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white z-50">
       <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-b-4 border-white"></div>
+      <div className=' text-4xl'> Please wait, the backend server might be reloading</div>
     </div>
   );
 };
